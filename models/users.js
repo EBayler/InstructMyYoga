14f0e3d612f76f8b58c185d456a15ccf0b374f90
gardnern <48269958+gardnern@users.noreply.github.com>
2019-08-08 19:39:21 -0600

    Update package-lock.json

--- models/users.js
+++ models/users.js
@@ -2,9 +2,29 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  name: { type: String, required: true },
-  email: String,
-  });
+  
+  username: { 
+    type: String, 
+    trim: true,
+    required: "Username is Required",
+  },
+  password: {
+    type: String,
+    trim: true,
+    required: "Password is Required",
+    validate: [
+      function(input) {
+        return input.length >= 6;
+      },
+      "Password should be longer"
+    ]
+  }, 
+  email: {
+    type: String,
+    unique: true,
+    match: [/.+@.+\..+/, "Please enter a valid e-mail address"]
+  }
+});
 
 
 const User = mongoose.model("User", userSchema);
