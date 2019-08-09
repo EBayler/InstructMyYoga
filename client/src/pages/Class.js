880bbe169a320b3f2aead9d50f0f6ec7db4eeb2e
NunyaBizness <jmclark801@yahoo.com>
2019-08-08 20:44:13 -0600

    Add route info

--- client/src/pages/Class.js
+++ client/src/pages/Class.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react';
 import PoseCard from '../components/PoseCard'
+import API from "../utils/API";
 
 class Class extends Component {
+
+  state = {
+    poses: []
+  }
+
+  componentDidMount(){
+    this.loadPoses();
+  }
+
+  loadPoses = () => {
+    API.getPoses()
+    .then(res => {
+      this.setState({
+        poses: res.data
+      }) 
+      console.log(this.state.poses);
+    });
+  }
   render(){
     return (
-      <PoseCard>This is a PoseCard .children prop</PoseCard>
+      <div>
+        <PoseCard>This is a PoseCard .children prop</PoseCard>
+      </div>
+      
     );
   }
 
--- routes/api/index.js
+++ routes/api/index.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const poseRoutes = require("./poses");
 
 // Should this use poses, class or something else? 
-router.use("/pose", poseRoutes);
+router.use("/poses", poseRoutes);
 //router.use("/books", bookRoutes);
 
 module.exports = router;
