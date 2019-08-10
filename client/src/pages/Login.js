90b6343b8f62195d6454d6a0c0d0f4492f6ded7b
gardnern <48269958+gardnern@users.noreply.github.com>
2019-08-10 11:38:09 -0600

    deleted FB codes and added button to the right

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,5 +1,4 @@
 
-import FacebookLogin from 'react-facebook-login';
 // import GoogleLogin from 'react-google-login';
 import React from 'react';
 
@@ -9,44 +8,5 @@ const Login = props => {
     <h2>Login works!</h2>
   )
 }
-// FB.getLoginStatus(function(response) {
-//   statusChangeCallback(response) {
-//       'connected',
-//       authResponse: {
-//       accessToken: '...',
-//       expiresIn:'...',
-//       signedRequest:'...',
-//       userID:'...'
-//   },
-//   });
 
-//   function checkLoginState() {
-//     FB.getLoginStatus(function(response) {
-//       statusChangeCallback(response);
-//     });
-//   }
-
-
-// const responseFacebook = (response) => {
-//   console.log(response);
-// }
-
-
-// class Login extends React.Component {
-//   render(){
-//     return(
-//       <div>This is the login page
-//         <FacebookLogin
-//           appId="1088597931155576"
-//           autoLoad={true}
-//           fields="name,email,picture"
-//           callback={responseFacebook}
-//           cssClass="my-facebook-button-class"
-//           icon="fa-facebook"
-//           >Login with facebook
-//         </FacebookLogin>
-//         </div>
-//     )
-//   } 
-// }
 export default Login
