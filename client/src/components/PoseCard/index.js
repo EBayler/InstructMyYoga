c99e169fc3d8b3fca5029cb9729c2c9278b70a60
EBayler <erika.bayler@gmail.com>
2019-08-16 16:30:01 -0600

    flip card feature

--- client/src/components/PoseCard/index.js
+++ client/src/components/PoseCard/index.js
@@ -15,13 +15,19 @@ function PoseCard(props){
 
   return (
     <div className="PoseCard">'
-      <img className="poseImage" alt={`${props.name}`} src={process.env.PUBLIC_URL + `${props.image}`}></img>
+      <div className="PoseCard-inner">
+        <div className="PoseCard-front">
+        <img className="poseImage" alt={`${props.name}`} src={process.env.PUBLIC_URL + `${props.image}`}></img>
+        </div>
+      <div className="PoseCard-back">
       <p><strong>Name: </strong> {props.name}</p>
       <p>Sanskrit Name: {props.sanskrit}</p>
       <p>Type: {props.type}</p>
       <p>Benefits: {benefitsList}</p>
       <p>Difficulty: {props.difficulty}</p>
-      <p>{props.description}</p>
+          <p>{props.description}</p>
+          </div>
+        </div>
     </div>
   );
 }
